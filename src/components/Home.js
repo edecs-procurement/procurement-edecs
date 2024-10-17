80933e6de41d255d564a19347d0a40f9135b2a05
edecs-procurement <edecs.procurement@gmail.com>
2024-10-17 14:33:20 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,12 +12,18 @@ const Home = () => {
       try {
         const response = await axios.get('https://procurement-edecs-default-rtdb.firebaseio.com/vendors.json');
         
+        // Check if data is received
+        console.log('Raw data from Firebase:', response.data);
+
         // Map the data to extract vendor info
         const vendorsData = Object.entries(response.data).map(([id, data]) => ({
           id,
           ...data.vendorInfo,  // Spread the vendorInfo properties
         }));
-        
+
+        // Log the mapped vendor data
+        console.log('Mapped vendors data:', vendorsData);
+
         setVendors(vendorsData);
       } catch (error) {
         console.error('حدث خطأ أثناء جلب البيانات:', error);
