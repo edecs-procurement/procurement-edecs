0e502cec1781dfee28bef54e81b78b03f2d8428f
edecs-procurement <edecs.procurement@gmail.com>
2024-10-17 21:31:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import VendorDetail from './components/VendorDetail';
+import Login from './components/Login'; // استيراد مكون Login
 import Navbar from './components/Navbar'; // استيراد Navbar
 import { LoadingProvider } from './LoadingContext'; // استيراد LoadingProvider
 import Loading from './components/Loading'; // استيراد مكون التحميل
@@ -12,7 +14,8 @@ function App() {
       <Router>
         <Navbar /> {/* إضافة Navbar */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} /> {/* تعيين صفحة تسجيل الدخول كصفحة رئيسية */}
+          <Route path="/home" element={<Home />} /> {/* تغيير مسار صفحة الهوم */}
           <Route path="/vendor/:id" element={<VendorDetail />} />
         </Routes>
         <Loading /> {/* عرض مكون التحميل */}
