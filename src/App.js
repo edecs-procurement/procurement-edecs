84dd18422e5d5a8626b0a32345620335fa1f4cbe
edecs-procurement <edecs.procurement@gmail.com>
2024-10-20 10:10:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import VendorDetail from './components/VendorDetail';
-import Login from './components/Login'; // استيراد مكون Login
-import Navbar from './components/Navbar'; // استيراد Navbar
-import { LoadingProvider } from './LoadingContext'; // استيراد LoadingProvider
-import Loading from './components/Loading'; // استيراد مكون التحميل
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import VendorDetail from "./components/VendorDetail";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import { LoadingProvider } from "./LoadingContext";
+import Loading from "./components/Loading";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // حالة تسجيل الدخول
+
   return (
     <LoadingProvider>
       <Router>
-        <Navbar /> {/* إضافة Navbar */}
+        <Navbar isLoggedIn={isLoggedIn} />{" "}
+        {/* تمرير حالة تسجيل الدخول إلى Navbar */}
         <Routes>
-          <Route path="/" element={<Login />} /> {/* تعيين صفحة تسجيل الدخول كصفحة رئيسية */}
-          <Route path="/home" element={<Home />} /> {/* تغيير مسار صفحة الهوم */}
+          <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />{" "}
+          {/* تمرير دالة تحديث حالة تسجيل الدخول إلى Login */}
+          <Route path="/home" element={<Home />} />
           <Route path="/vendor/:id" element={<VendorDetail />} />
         </Routes>
-        <Loading /> {/* عرض مكون التحميل */}
+        <Loading />
       </Router>
     </LoadingProvider>
   );
